b960fe40ce67abbd52dfd1e73ca4bb13f8583c22
satyajit nayak <satyajitn15@gmail.com>
2023-01-15 13:55:59 +0530

    Add user login api.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, JwtService],
 })
 export class UsersModule {}
